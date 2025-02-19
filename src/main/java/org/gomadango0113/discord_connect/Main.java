51b0355e44bd569825509009b1b3b84a7327fb71
gomadango0113 <gomadango0113@gmail.com>
2025-02-19 15:58:07 +0900

    first commit

---
+++ src/main/java/org/gomadango0113/discord_connect/Main.java
@@ -0,0 +1,17 @@
+package org.gomadango0113.discord_connect;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
